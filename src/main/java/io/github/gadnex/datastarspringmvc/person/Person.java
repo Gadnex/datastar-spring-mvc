082e9242a5b396af1935a03d0ca17afade66777c
William Gadney <william.gadney@mendesgans.nl>
2025-01-29 19:09:49 +0100

    Add localization support for Person example

--- src/main/java/io/github/gadnex/datastarspringmvc/person/Person.java
+++ src/main/java/io/github/gadnex/datastarspringmvc/person/Person.java
@@ -4,4 +4,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record Person(@NotBlank @Size(min = 3) String name, @NotBlank @Email String email) {}
+public record Person(
+    @NotBlank(message = "{Person.name.notBlank}") @Size(min = 3, message = "{Person.name.size}")
+        String name,
+    @NotBlank(message = "{Person.email.notBlank}") @Email(message = "{Person.email.email}")
+        String email) {}
