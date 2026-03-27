7412927f12b60bc464fb75509372d002f7fbe524
William Gadney <gadnex@gmail.com>
2026-03-27 15:12:56 +0100

    Upgrade to Spring Boot 4.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import net.ltgt.gradle.errorprone.errorprone
 
 plugins {
     java
-    id("org.springframework.boot") version "4.0.4"
+    id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
     id("io.spring.nullability") version "0.0.12"
     id("org.graalvm.buildtools.native") version "0.11.5"
